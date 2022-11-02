9cf254049f316fe028a421ce93376d378ce5df6f
Shubham kumar <kumarshubham141200@gmail.com>
2022-11-02 13:25:48 +0530

    linked lists

--- Linked Lists/Reverse Linked list.py
+++ Linked Lists/Reverse Linked list.py
@@ -23,3 +23,21 @@ class Solution:
             prev = current
             current = temp
         return prev
+
+
+# RECURSIVE SOLUTION
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        def reverse(curr, prev):
+            if curr is None:
+                return prev
+            else:
+                temp = curr.next
+                curr.next = prev
+                return reverse(temp, curr)
+        return reverse(head, None)
